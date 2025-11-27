4f6cf40b6c1ae3f1cc81f7372959af285730a4a4
RanXom <saiyedshizain@gmail.com>
2025-11-27 10:43:05 +0000

    refactor(repo): update CaseRepository to align with new entity relations

--- src/main/java/com/ranxom/cdms/repository/CaseRepository.java
+++ src/main/java/com/ranxom/cdms/repository/CaseRepository.java
@@ -4,6 +4,10 @@ import com.ranxom.cdms.model.Cases;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CaseRepository extends JpaRepository<Cases, Integer> {
+public interface CaseRepository extends JpaRepository<Cases, Long> {
+    List<Cases> findByStatus(String status);
+    List<Cases> findByLeadOfficer_OfficersId(Long officerId);
 }
